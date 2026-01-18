e0dae456e7e61e583502a191b31ad0e6bc163c00
Heorhii Ponomarenko <ge.ponomarenko@gmail.com>
2026-01-18 15:05:58 +0100

    Added two models and made migration

--- collection/models.py
+++ collection/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+class Game(models.Model):
+    name = models.CharField(max_length=500)
+    genre = models.ManyToManyField(Genre)
+    release_date = models.DateField()
+    rating = models.IntegerField()
+    
