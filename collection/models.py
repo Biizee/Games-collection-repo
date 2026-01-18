590f83ce27e4334ad9b44dfe9abc2959d7bc71c7
Heorhii Ponomarenko <ge.ponomarenko@gmail.com>
2026-01-18 15:25:11 +0100

    Added magic method str to models, added some games and genres to db and added more functional to main.py

--- collection/models.py
+++ collection/models.py
@@ -5,9 +5,14 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Game(models.Model):
     name = models.CharField(max_length=500)
     genre = models.ManyToManyField(Genre)
     release_date = models.DateField()
     rating = models.IntegerField()
-    
+
+    def __str__(self):
+        return self.name
--- main.py
+++ main.py
@@ -1,4 +1,63 @@
 import django_setup
 
-#from collection.models import
+from collection.models import Genre, Game
 
+#!--Creating some genres of games
+#first_genre = Genre.objects.create(
+#    name = "JRPG"
+#)
+#
+#second_genre = Genre.objects.create(
+#    name = "Roguelike"
+#)
+#
+#third_genre = Genre.objects.create(
+#    name = "Shooter"
+#)
+
+#!--Getting genres by their id
+first_genre = Genre.objects.get(id = 1)
+second_genre = Genre.objects.get(id = 2)
+third_genre = Genre.objects.get(id = 3)
+
+#!--Creating some games
+#first_game = Game.objects.create(
+#    name = "Heaven for Death",
+#    release_date = "2023-01-27",
+#    rating = 9
+#)
+#
+#second_game = Game.objects.create(
+#    name = "Omori",
+#    release_date = "2020-12-25",
+#    rating = 10
+#)
+#
+#third_game = Game.objects.create(
+#    name = "Risk of Rain 2",
+#    release_date = "2020-03-28",
+#    rating = 8
+#)
+#
+#fourth_game = Game.objects.create(
+#    name = "Ultrakill",
+#    release_date = "2020-09-03",
+#    rating = 9
+#)
+
+#!--Getting games by their id
+first_game = Game.objects.get(id = 1)
+second_game = Game.objects.get(id = 2)
+third_game = Game.objects.get(id = 3)
+fourth_game = Game.objects.get(id = 4)
+
+#!--Relating genres and games
+#first_game.genre.add(first_genre)
+#second_game.genre.add(first_genre)
+#third_game.genre.add(second_genre)
+#fourth_game.genre.add(third_genre)
+
+#!--Printing info about games and their genres
+games = Game.objects.all()
+for g in games:
+    print(g.name + " гра у жанрі " + str(g.genre.all()))
